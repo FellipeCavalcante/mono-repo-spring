b0cc224fc2f68b71eca35a8145111f9b26a7b6ab
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-11-29 18:17:28 -0300

    initial commit of the project

---
+++ anime-service/src/test/java/com/dev/fellipe/anime_service/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.fellipe.anime_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
