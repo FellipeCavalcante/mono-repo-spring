fc2c0be7bd10ddacab6b37b06e26e769c24c671c
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-12-02 16:57:07 -0300

    post

--- anime-service/src/main/java/com/dev/fellipe/anime_service/domain/Anime.java
+++ anime-service/src/main/java/com/dev/fellipe/anime_service/domain/Anime.java
@@ -1,23 +1,20 @@
 package com.dev.fellipe.anime_service.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class Anime {
     private Long id;
     private String name;
 
-    public Anime(String name, Long id) {
-        this.name = name;
-        this.id = id;
-    }
-
     public static List<Anime> getAnime() {
-        var itachi = new Anime("Itachi", 1L);
-        var gojou = new Anime("Satoru Gojou", 2L);
-        var tanjiro = new Anime("Tanjiro", 3L);
+        var itachi = new Anime(1L, "Itachi");
+        var gojou = new Anime(2L, "Satoru Gojou");
+        var tanjiro = new Anime(3L, "Tanjiro");
 
         return List.of(itachi, gojou, tanjiro);
     }
