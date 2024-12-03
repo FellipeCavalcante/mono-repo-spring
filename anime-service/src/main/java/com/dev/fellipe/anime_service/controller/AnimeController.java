bdec1667ba01e0bfd5f25c7b733c0097edcad7b0
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-12-03 11:22:14 -0300

    animes post

--- anime-service/src/main/java/com/dev/fellipe/anime_service/controller/AnimeController.java
+++ anime-service/src/main/java/com/dev/fellipe/anime_service/controller/AnimeController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController()
 @RequestMapping("v1/animes")
@@ -13,7 +14,7 @@ public class AnimeController {
 
     @GetMapping()
     public List<Anime> listAll(@RequestParam(required = false) String name) {
-        var anime = Anime.getAnime();
+        var anime = Anime.getAnimes();
 
         if (name != null) {
             return anime.stream().filter(animes -> animes.getName().equalsIgnoreCase(name)).toList();
@@ -25,11 +26,16 @@ public class AnimeController {
 
     @GetMapping("{id}")
     public Anime findById(@PathVariable Long id) {
-        return Anime.getAnime()
+        return Anime.getAnimes()
                 .stream().filter(animes -> animes.getId().equals(id))
                 .findFirst().orElse(null);
 
     }
 
-
+    @PostMapping
+    public Anime save(@RequestBody Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(100_000));
+        Anime.getAnimes().add(anime);
+        return anime;
+    }
 }
--- anime-service/src/main/java/com/dev/fellipe/anime_service/domain/Anime.java
+++ anime-service/src/main/java/com/dev/fellipe/anime_service/domain/Anime.java
@@ -2,20 +2,26 @@ package com.dev.fellipe.anime_service.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class Anime {
     private Long id;
     private String name;
-
-    public static List<Anime> getAnime() {
+    private static List<Anime> animes = new ArrayList<>();
+    static {
         var itachi = new Anime(1L, "Itachi");
         var gojou = new Anime(2L, "Satoru Gojou");
         var tanjiro = new Anime(3L, "Tanjiro");
+        animes.addAll(List.of(itachi, gojou, tanjiro));
+    }
 
-        return List.of(itachi, gojou, tanjiro);
+    public static List<Anime> getAnimes() {
+        return animes;
     }
 }
