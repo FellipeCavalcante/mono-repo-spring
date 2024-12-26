90b7eca5559983cd5c7203d280d3ea6df4c82db2
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-12-26 17:28:32 -0300

    produce: fixed

--- anime-service/src/main/java/com/dev/fellipe/anime_service/controller/ProducersController.java
+++ anime-service/src/main/java/com/dev/fellipe/anime_service/controller/ProducersController.java
@@ -11,9 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 @RestController()
 @RequestMapping("v1/producers")
