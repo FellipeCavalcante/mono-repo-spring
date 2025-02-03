b8ae322e5d04fa8f8af14a0b409003365a2d073f
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-02-03 09:42:02 -0300

    producer: MVC 1

--- anime-service/src/main/java/com/dev/fellipe/anime_service/domain/Producer.java
+++ anime-service/src/main/java/com/dev/fellipe/anime_service/domain/Producer.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
@@ -17,16 +15,5 @@ public class Producer {
     @JsonProperty("name")
     private String name;
     private LocalDateTime createdAt;
-    private static List<Producer> producers = new ArrayList<>();
 
-    static {
-        var mappa = Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build();
-        var kyotoAnimation = Producer.builder().id(2L).name("Kyoto Animation").createdAt(LocalDateTime.now()).build();
-        var madhouse = Producer.builder().id(3L).name("Madhouse").createdAt(LocalDateTime.now()).build();
-        producers.addAll(List.of(mappa, madhouse, kyotoAnimation));
-    }
-
-    public static List<Producer> getProducers() {
-        return producers;
-    }
 }
---
+++ anime-service/src/main/java/com/dev/fellipe/anime_service/repository/ProduceHardCodeRepository.java
@@ -0,0 +1,45 @@
+package com.dev.fellipe.anime_service.repository;
+
+import com.dev.fellipe.anime_service.domain.Producer;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ProduceHardCodeRepository {
+    private static final List<Producer> PRODUCERS = new ArrayList<>();
+
+    static {
+        var mappa = com.dev.fellipe.anime_service.domain.Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build();
+        var kyotoAnimation = com.dev.fellipe.anime_service.domain.Producer.builder().id(2L).name("Kyoto Animation").createdAt(LocalDateTime.now()).build();
+        var madhouse = com.dev.fellipe.anime_service.domain.Producer.builder().id(3L).name("Madhouse").createdAt(LocalDateTime.now()).build();
+        PRODUCERS.addAll(List.of(mappa, madhouse, kyotoAnimation));
+    }
+
+    public static List<Producer> findAll() {
+        return PRODUCERS;
+    }
+
+    public Optional<Producer> findById(Long id) {
+        return PRODUCERS.stream().filter(producer -> producer.getId().equals(id)).findFirst();
+    }
+
+    public List<Producer> findByName(String name) {
+        return PRODUCERS.stream().filter(producer -> producer.getName().equalsIgnoreCase(name)).toList();
+    }
+
+    public Producer save(Producer producer) {
+        PRODUCERS.add(producer);
+        return producer;
+    }
+
+    public void delete(Producer producer) {
+        PRODUCERS.remove(producer);
+    }
+
+    public void update(Producer producer) {
+        delete(producer);
+        save(producer);
+    }
+}
