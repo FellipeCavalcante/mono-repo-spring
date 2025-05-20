3c6023e0dd78e8d253454534cbef71357184d036
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-05-20 18:31:27 -0300

    spring JPA in anime-service

--- anime-service/src/main/java/com/dev/fellipe/anime_service/AnimeServiceApplication.java
+++ anime-service/src/main/java/com/dev/fellipe/anime_service/AnimeServiceApplication.java
@@ -1,12 +1,9 @@
 package com.dev.fellipe.anime_service;
 
-import com.dev.fellipe.anime_service.config.ConnectionConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ConnectionConfigurationProperties.class)
 public class AnimeServiceApplication {
 
     public static void main(String[] args) {
--- anime-service/src/main/java/com/dev/fellipe/anime_service/config/ConnectionConfigurationProperties.java
+++
@@ -1,7 +0,0 @@
-package com.dev.fellipe.anime_service.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "database")
-public record ConnectionConfigurationProperties(String url, String username, String password) {
-}
