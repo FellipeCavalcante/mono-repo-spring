42531e224ecff1b351fcc0d9012f9f25f2f3d8ce
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-08-04 13:25:15 -0300

    feat: query with jpql

--- user-service/src/main/java/com/dev/fellipe/user_service/repository/UserProfileRepository.java
+++ user-service/src/main/java/com/dev/fellipe/user_service/repository/UserProfileRepository.java
@@ -2,6 +2,13 @@ package com.dev.fellipe.user_service.repository;
 
 import com.dev.fellipe.user_service.domain.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+
+    @Override
+    @Query("SELECT up FROM UserProfile up join fetch up.user u join fetch up.profile p")
+    List<UserProfile> findAll();
 }
