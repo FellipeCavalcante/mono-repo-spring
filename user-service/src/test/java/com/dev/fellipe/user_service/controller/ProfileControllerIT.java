db8499fcd616c22d7bcdb5bb71f3166e96754f63
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-08-04 22:30:35 -0300

    fix: profile integration test

---
+++ user-service/src/test/java/com/dev/fellipe/user_service/controller/ProfileControllerIT.java
@@ -0,0 +1,40 @@
+package com.dev.fellipe.user_service.controller;
+
+import com.dev.fellipe.user_service.response.ProfileGetResponse;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ProfileControllerIT {
+    private static final String URL = "/v1/profiles";
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    @DisplayName("GET v1/profiles returns a list with all profiles successful")
+    @Sql(value = "/sql/init_two_profiles.sql")
+    @Order(1)
+    void findAll_ReturnsAllProfiles_WhenSuccessful() throws Exception {
+        var typeReference = new ParameterizedTypeReference<List<ProfileGetResponse>>() {};
+
+        var responseEntity = testRestTemplate.exchange(URL, HttpMethod.GET, null, typeReference);
+        Assertions.assertThat(responseEntity).isNotNull();
+        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(responseEntity.getBody()).isNotNull().doesNotContainNull();
+
+        responseEntity
+                .getBody()
+                .forEach(profileGetResponse -> Assertions.assertThat(profileGetResponse).hasNoNullFieldsOrProperties());
+    }
+}
---
+++ user-service/src/test/resources/sql/init_two_profiles.sql
@@ -0,0 +1,2 @@
+insert into profile (id, description, name) values (1, 'Manager everything', 'Admin');
+insert into profile (id, description, name) values (2, 'Regular viewer', 'Viewer');
