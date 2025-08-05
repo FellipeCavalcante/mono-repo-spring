eac7b0adab07940aabaf678cb4ae291d1269df5c
FellipeCavalcante <azevedoaranha1@gmail.com>
2025-08-04 22:57:56 -0300

    fix: profile integration test testRestTemplate

--- user-service/src/main/java/com/dev/fellipe/user_service/response/ProfilePostResponse.java
+++ user-service/src/main/java/com/dev/fellipe/user_service/response/ProfilePostResponse.java
@@ -1,12 +1,12 @@
 package com.dev.fellipe.user_service.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProfilePostResponse {
     private Long id;
 }
--- user-service/src/test/java/com/dev/fellipe/user_service/controller/ProfileControllerIT.java
+++ user-service/src/test/java/com/dev/fellipe/user_service/controller/ProfileControllerIT.java
@@ -1,14 +1,17 @@
 package com.dev.fellipe.user_service.controller;
 
+import com.dev.fellipe.user_service.commons.ProfileUtils;
+import com.dev.fellipe.user_service.domain.Profile;
 import com.dev.fellipe.user_service.response.ProfileGetResponse;
+import com.dev.fellipe.user_service.response.ProfilePostResponse;
+import com.dev.fellipe.user_service.commons.FIleUtis;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
@@ -20,6 +23,10 @@ public class ProfileControllerIT {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
+    @Autowired
+    private ProfileUtils profileUtils;
+    @Autowired
+    private FIleUtis fileUtils;
 
     @Test
     @DisplayName("GET v1/profiles returns a list with all profiles successful")
@@ -36,5 +43,39 @@ public class ProfileControllerIT {
         responseEntity
                 .getBody()
                 .forEach(profileGetResponse -> Assertions.assertThat(profileGetResponse).hasNoNullFieldsOrProperties());
+    }
+
+    @Test
+    @DisplayName("GET v1/profiles returns empty list when nothing is not found")
+    @Order(2)
+    void findByName_ReturnEmptyList_WhenNothingIsFound() throws Exception {
+        var typeReference = new ParameterizedTypeReference<List<ProfileGetResponse>>() {};
+
+        var responseEntity = testRestTemplate.exchange(URL, HttpMethod.GET, null, typeReference);
+        Assertions.assertThat(responseEntity).isNotNull();
+        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(responseEntity.getBody()).isNotNull().isEmpty();
+    }
+
+    @Test
+    @DisplayName("POST v1/profiles creates a profile")
+    @Order(3)
+    void save_CreatesProducer_WhenSuccessful() throws Exception {
+        var request = fileUtils.readResourceFile("profile/post-request-profile-200.json");
+        var profileEntity = buildHttpEntity(request);
+
+        var responseEntity = testRestTemplate.exchange(URL, HttpMethod.POST, profileEntity, ProfilePostResponse.class);
+
+        Assertions.assertThat(responseEntity).isNotNull();
+        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        Assertions.assertThat(responseEntity.getBody()).isNotNull().hasNoNullFieldsOrProperties();
+    }
+
+    private static HttpEntity<String> buildHttpEntity(String request) {
+        var httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        var profileJson = new HttpEntity<>(request, httpHeaders);
+        return profileJson;
     }
 }
